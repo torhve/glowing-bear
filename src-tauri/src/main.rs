46d431ead7478e3b37aac98fe5ce4266664eca77
Tor Hveem <tor@hveem.no>
2026-06-20 13:20:18 +0200

    fix tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,9 @@ fn main() {
     .plugin(tauri_plugin_window_state::Builder::new().build())
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_notification::init())
-    .plugin(tauri_plugin_autostart::init())
+    .plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent,
+            Some(vec![""])
+        ))
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
