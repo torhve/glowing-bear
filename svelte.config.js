7a4a17e8a8191f27aee3468f5db29838323cd3b3
Tor Hveem <tor@hveem.no>
2026-06-28 13:40:14 +0200

    feat: auto-deploy web frontend to GitHub Pages on release

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
+		paths: {
+			base: process.env.BASE_PATH ?? '',
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
